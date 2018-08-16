4b591cd7cb6ce164722b15936aefadf4b7f44100
Robbie Moyer <rmoyer08@gmail.com>
2018-08-16 11:11:05 -0400

    Data decoded() unit tests

--- Tests/FoundationTests/DataExtensionsTests.swift
+++ Tests/FoundationTests/DataExtensionsTests.swift
@@ -11,6 +11,12 @@ import XCTest
 
 final class DataExtensionsTests: XCTestCase {
 
+    private struct TestDecodableObject: Equatable, Decodable {
+        let firstProperty: Int
+        let secondProperty: String
+        let thirdProperty: Bool
+    }
+    
 	func testString() {
 		let dataFromString = "hello".data(using: .utf8)
 		XCTAssertNotNil(dataFromString)
@@ -23,6 +29,69 @@ final class DataExtensionsTests: XCTestCase {
         let bytes = dataFromString?.bytes
         XCTAssertNotNil(bytes)
         XCTAssertEqual(bytes?.count, 5)
+    }
+    
+    func testDecoded() {
+        let jsonData =
+        """
+        {
+            "firstProperty": 100,
+            "secondProperty": "Hello World!",
+            "thirdProperty": true
+        }
+        """.data(using: .utf8)
+        
+        let plistData =
+        """
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+            <key>firstProperty</key>
+            <integer>100</integer>
+            <key>secondProperty</key>
+            <string>Hello World!</string>
+            <key>thirdProperty</key>
+            <true/>
+        </dict>
+        </plist>
+        """.data(using: .utf8)
+        
+        let invalidJSON = "{ badJSON }".data(using: .utf8)
+        let invalidPLIST = "<key>badPLIST</key>".data(using: .utf8)
+        
+        // Verify data was created
+        
+        XCTAssertNotNil(jsonData)
+        XCTAssertNotNil(plistData)
+        XCTAssertNotNil(invalidJSON)
+        XCTAssertNotNil(invalidPLIST)
+        
+        // Test decoding from JSON
+        
+        let explicitFromJSON = try? jsonData!.decoded() as TestDecodableObject
+        let implicitFromJSON: TestDecodableObject? = try? jsonData!.decoded()
+        
+        XCTAssertNotNil(explicitFromJSON)
+        XCTAssertNotNil(implicitFromJSON)
+        
+        XCTAssertEqual(explicitFromJSON, implicitFromJSON)
+        
+        XCTAssertThrowsError(try invalidJSON!.decoded() as TestDecodableObject)
+        
+        // Test decoding from PLIST
+        
+        let plistDecoder = PropertyListDecoder()
+        
+        let explicitFromPLIST = try? plistData!.decoded(using: plistDecoder) as TestDecodableObject
+        let implicitFromPLIST: TestDecodableObject? = try? plistData!.decoded(using: plistDecoder)
+        
+        XCTAssertNotNil(explicitFromPLIST)
+        XCTAssertNotNil(implicitFromPLIST)
+        
+        XCTAssertEqual(explicitFromPLIST, implicitFromPLIST)
+        
+        XCTAssertThrowsError(try invalidPLIST!.decoded(using: plistDecoder) as TestDecodableObject)
     }
 
 }
